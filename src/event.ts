400730e504b733abd8feedf2d42cdd5630ac28c3
Sebastian Bean <seb@bean.la>
2025-05-06 03:30:06 -0700

    event type

--- src/event.ts
+++ src/event.ts
@@ -1,11 +1,13 @@
-export enum Event {
-    Enqueue = "enqueue",
-    Start = "start",
-    Failure = "failure",
-    Complete = "complete",
-    Remove = "remove",
-    Error = "error",
-    Progress = "progress",
-    Log = "log",
-    Priority = "priority",
-}
+export const Event = {
+    Enqueue: "enqueue",
+    Start: "start",
+    Failure: "failure",
+    Complete: "complete",
+    Remove: "remove",
+    Error: "error",
+    Progress: "progress",
+    Log: "log",
+    Priority: "priority",
+} as const
+
+export type Event = typeof Event[keyof typeof Event]
